abe8485216ca688e7a64bfe2d65dd564928de8b8
Sunguk Lee <d3m3vilurr@gmail.com>
2015-03-31 02:47:11 +0900

    Z_PROBE_OFFSET_FROM_EXTRUDER 0.3 sometimes cause nozzle hang

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -498,7 +498,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 0       // Probe on: -left  +right
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0       // Probe on: -front +behind
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER 0.3     // -below (always!)
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER 0.4     // -below (always!)
 
   #define Z_RAISE_BEFORE_HOMING 15       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
